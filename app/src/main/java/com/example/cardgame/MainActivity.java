8543e4cde73921953a993d7399ffd996d9bba5e5
Ben533 <benben472@gmail.com>
2020-11-11 21:42:28 +0200

    Ben: Updated app icon and name and moving to winner activity when run out of cards

--- app/src/main/java/com/example/cardgame/MainActivity.java
+++ app/src/main/java/com/example/cardgame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cardgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +17,7 @@ public class MainActivity extends Activity {
     private ImageView rightCardImg;
     private int rightPlayerScore = 0;
     private int leftPlayerScore = 0;
+    private int draws = 0;
 
 
     @Override
@@ -56,37 +58,50 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.mian_btn_play:
-                    // Get top cards.
-                    String[] topCards = cardsDeck.getTopCards();
-                    Log.d(CardsDeck.TAG, topCards[0] + " | " + topCards[1]);
+                    if (rightPlayerScore + leftPlayerScore + draws == 26)
+                        openActivity(MainActivity.this);
+                    else {
+                        // Get top cards:
+                        String[] topCards = cardsDeck.getTopCards();
+                        Log.d(CardsDeck.TAG, topCards[0] + " | " + topCards[1]);
 
-                    // Show cards.
-                    leftCardImg.setImageResource(getResources().getIdentifier(topCards[0], "drawable", getPackageName()));
-                    rightCardImg.setImageResource(getResources().getIdentifier(topCards[1], "drawable", getPackageName()));
+                        // Show cards.
+                        leftCardImg.setImageResource(getResources().getIdentifier(topCards[0], "drawable", getPackageName()));
+                        rightCardImg.setImageResource(getResources().getIdentifier(topCards[1], "drawable", getPackageName()));
 
-                    // Get winner.
-                    switch (CardsDeck.getWinner(topCards[0], topCards[1])) {
-                        case 0:
-                            // Do nothing.
-                            break;
-                        case 1:
-                            leftPlayerScore ++;
-                            leftScore.setText("" + leftPlayerScore);
-                            break;
-                        case 2:
-                            rightPlayerScore ++;
-                            rightScore.setText("" + rightPlayerScore);
-                            break;
+                        // Get winner.
+                        switch (CardsDeck.getWinner(topCards[0], topCards[1])) {
+                            case 0:
+                                // Number of draws + 1
+                                draws++;
+                                break;
+                            case 1:
+                                leftPlayerScore++;
+                                leftScore.setText("" + leftPlayerScore);
+                                break;
+                            case 2:
+                                rightPlayerScore++;
+                                rightScore.setText("" + rightPlayerScore);
+                        }
                     }
                     break;
             }
         }
     };
+
+    private void openActivity(Activity activity) {
+        Intent myIntent = new Intent(activity, Winner_Activity.class);
+        startActivity(myIntent);
+    }
+
 }
-
-
